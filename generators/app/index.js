475439ac9b673baf149a148721934ade872bb882
sthzg <sthzg@gmx.net>
2016-07-04 19:03:17 +0200

    Updated app generator to create a few files

--- generators/app/index.js
+++ generators/app/index.js
@@ -5,10 +5,19 @@ const generators                  = require('yeoman-generator');
 
 class Generator extends generators.Base {
 
-  get default() {
+  get initializing() {
     return {
       output: function() {
         this.log('Welcome at Yo Burger!');
+      }
+    }
+  }
+
+  get writing() {
+    return {
+      output: function() {
+        this.log(this.templatePath(), this.destinationPath());
+        this.fs.copy(this.templatePath(), this.destinationPath());
       }
     }
   }
---
+++ generators/app/templates/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+
+module.exports = {
+  sayHello: function() {
+    console.log('Hello, Burger');
+  }
+};
